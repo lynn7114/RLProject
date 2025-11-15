e74e50c8f14791414fb9923ec6d8e8609f8c7616
lynn7114 <154987575+lynn7114@users.noreply.github.com>
2025-11-15 02:49:57 +0000

    Add RL project structure and scripts

--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,3 @@
 import numpy as np
 
-def evaluate(model, env, n_episodes=5):
-    total_rewards = []
-    for episode in range(n_episodes):
-        obs, _ = env.reset()
-        done = False
-        total_reward = 0
-        while not done:
-            action, _ = model.predict(obs)
-            obs, reward, done, _, _ = env.step(action)
-            total_reward += reward
-        total_rewards.append(total_reward)
-    return np.mean(total_rewards), np.std(total_rewards)
+
